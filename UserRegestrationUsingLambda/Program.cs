1a62ca43dc15b252789846b172991915ce43de3c
Vaibhav Mehta <Vaibhavmehta524@gmail.com>
2023-03-22 03:48:46 +0530

    Modified to Validate Last Name using Lambda

--- UserRegestrationUsingLambda/Program.cs
+++ UserRegestrationUsingLambda/Program.cs
@@ -8,13 +8,16 @@ namespace UserRegestrationUsingLambda
         {
             Console.WriteLine("Welcome to User Registration Using Lambda Expression");
             Console.WriteLine("Please enter an option");
-            Console.WriteLine("1:First Name");
+            Console.WriteLine("1:First Name\n2:Check Last Name");
             int option = Convert.ToInt32(Console.ReadLine());
             UserRegistration userRegistration = new UserRegistration();
             switch (option)
             {
                 case 1:
-                    userRegistration.CheckFirstName("Shivani");
+                    userRegistration.CheckFirstName("Vaibhav");
+                    break;
+                case 2:
+                    userRegistration.CheckLastName("Mehta");
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
